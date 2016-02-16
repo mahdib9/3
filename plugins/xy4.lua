c608a8846fda84144fb2dca532ed811c7fc34176
Behrooz Yaghi <iranchild1@gmail.com>
2016-02-16 13:57:26 +0000

    Update xy4.lua

--- plugins/xy4.lua
+++ plugins/xy4.lua
@@ -1,12 +1,12 @@
 do
 local function run(msg, matches)
 local bot_id = our_id 
-local fbot1 = 191201331
+local fbot1 = 191201331 --ایدی اینویت کننده رو اینجا بنویسید
     if msg.action.type == "chat_del_user" and msg.action.user.id == tonumber(fbot1) then
        chat_add_user("chat#id"..msg.to.id, 'user#id'..fbot1, ok_cb, false)
     end
 end
- 
+ --channel : @nod32team
 return {
   patterns = {
     "^!!tgservice (.+)$",
@@ -14,3 +14,6 @@ return {
   run = run
 }
 end
+--open xy git by : @behroozyaghi
+--channel : @nod32team
+--کپی بدون ذکر منبع حرام است
